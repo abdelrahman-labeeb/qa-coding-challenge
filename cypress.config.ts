ac55f271df7edf47a6e2324026b9690b7776c813
Abdelrahman.Labeeb <59935036+abdelrahman-labeeb@users.noreply.github.com>
2025-08-30 21:05:23 +0200

    Add TODO for reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from "cypress";
-import installLogsPrinter from 'cypress-terminal-report/src/installLogsPrinter'
 
-
+// TODO: Add reporter
 export default defineConfig({
-    reporter: 'cypress-multi-reporters',
-    reporterOptions: {
-        reporterEnabled: 'spec, mocha-junit-reporter',
-        reportDir: 'cypress/results/reports',
-        mochaJunitReporterReporterOptions: {
-            mochaFile: 'results/junit/results-[hash].xml',
-            toConsole: true
-        }
-    },
     screenshotsFolder: 'cypress/results/screenshots',
     videosFolder: 'cypress/results/videos',
     retries: {
@@ -25,16 +15,6 @@ export default defineConfig({
         supportFile: "cypress/support/e2e.ts",
         env: {apiBaseUrl: "https://simple-books-api.click"},
         setupNodeEvents(on, config) {
-            console.log('[CTR] installLogsPrinter attached');
-            installLogsPrinter(on, {
-                printLogsToConsole: 'always',
-                includeSuccessfulHookLogs: false,
-                outputRoot: "results/ctr",
-                outputTarget: {
-                    "cypress-terminal-report.txt": "txt",
-                    "cypress-terminal-report.json": "json",
-                }
-            })
             return config
         }
     }
--- cypress/support/e2e.ts
+++
@@ -1,4 +0,0 @@
-console.log('[CTR] support file loaded')
-
-import 'cypress-terminal-report/src/installLogsCollector'
-import './commands'
