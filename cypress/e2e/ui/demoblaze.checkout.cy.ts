82b93c4354c7ad06f9a775551c5e3908aeecffbb
Abdelrahman.Labeeb <59935036+abdelrahman-labeeb@users.noreply.github.com>
2025-08-31 14:13:52 +0200

    Add test "purchase with empty form should fail"

--- cypress/e2e/ui/demoblaze.checkout.cy.ts
+++ cypress/e2e/ui/demoblaze.checkout.cy.ts
@@ -14,4 +14,20 @@ describe("Demoblaze checkout tests", () => {
             .then(() => cy.fillOrderInfo())
             .then(() => cy.submitOderAndConfirmSuccessfulPurchase());
     });
+
+    it("purchase with empty form should fail", () => {
+        cy.contains("#cartur", "Cart").should("be.visible").and("not.be.disabled");
+        cy.contains("#cartur", "Cart").click({ force: true });
+        cy.contains("button", "Place Order").should("be.visible").and("not.be.disabled");
+        cy.contains("button", "Place Order").click({ force: true });
+        cy.get('#orderModal').should('be.visible');
+        cy.contains("button", "Purchase").should("not.be.disabled");
+        cy.contains("button", "Purchase").click({ force: true });
+
+        cy.on('window:alert', function handler(txt: string) {
+            expect(txt).to.contain('Please fill out Name and Creditcard.');
+            cy.off('window:alert', handler); // removes after one trigger
+        });
+        cy.get("#orderModal").should("be.visible");
+    });
 });
